10400376d9066f5fc6f2d2b744c0495a3a851b45
issah255-Tech <gifteddaveservices@gmail.com>
2026-01-23 12:57:55 +0300

    Update WhatsApp Links

--- main.js
+++ main.js
@@ -197,7 +197,7 @@ async function handleMessages(sock, messageUpdate, printLog) {
             
             if (buttonId === 'channel') {
                 await sock.sendMessage(chatId, { 
-                    text: '📢 *Join our Channel:*\nhttps://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A' 
+                    text: '📢 *Join our Channel:*\nhttps://whatsapp.com/channel/0029VbC4f1K3GJOxckRrP026' 
                 }, { quoted: message });
                 return;
             } else if (buttonId === 'owner') {
@@ -206,7 +206,7 @@ async function handleMessages(sock, messageUpdate, printLog) {
                 return;
             } else if (buttonId === 'support') {
                 await sock.sendMessage(chatId, { 
-                    text: `🔗 *Support*\n\nhttps://chat.whatsapp.com/GA4WrOFythU6g3BFVubYM7?mode=wwt` 
+                    text: `🔗 *Support*\n\nhttps://chat.whatsapp.com/C8XiyjBfa9X6eNu1sYCSfu` 
                 }, { quoted: message });
                 return;
             }
