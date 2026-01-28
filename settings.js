0a1aecbb066513711f23b5665846511de756faae
Dave-Tech <babydave081@gmail.com>
2026-01-28 18:01:36 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const settings = {
   author: '‎',
   botName: "ISSA-XMD",
   botOwner: 'issa', // Your name
-  ownerNumber: '255778018545', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254104260236', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
-  updateZipUrl: "https://github.com/xmdissa23/issa-quantum/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/issah255-Tech/ISSA-XMD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
