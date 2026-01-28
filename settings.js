b6250a13f448f5b70b6442b5d94e82eba5ecd3fb
Dave-Tech <babydave081@gmail.com>
2026-01-28 18:00:58 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.5",
+  version: "1.0.0",
   updateZipUrl: "https://github.com/xmdissa23/issa-quantum/archive/refs/heads/main.zip",
 };
 
